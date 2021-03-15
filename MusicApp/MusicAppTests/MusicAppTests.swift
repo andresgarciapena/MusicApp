917acd9d5a38bd3866d48bd73062cb92614e3589
Andres Garcia <andres.garcia.p.91@gmail.com>
2021-03-15 08:48:37 +0100

    Test service call

--- MusicApp/MusicAppTests/MusicAppTests.swift
+++ MusicApp/MusicAppTests/MusicAppTests.swift
@@ -6,28 +6,35 @@
 //
 
 import XCTest
+import Alamofire
 @testable import MusicApp
 
 class MusicAppTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    
+    override func setUp() {
+        super.setUp()
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    override func tearDown() {
+        super.tearDown()
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    
+    func testValidCallToApi() {
+        
+        let url = "https://itunes.apple.com/search?media=music&term=taylor"
+        
+        let request = AF.request(url)
+        
+        request.responseDecodable(of: ItunesSongResponse.self) { (response) in
+          guard let _ = response.value else { return }
+            
+            if let statusCode = response.response?.statusCode {
+                if statusCode == 200 {
+                    XCTAssert(statusCode == 200)
+                } else {
+                    XCTFail("Status code: \(statusCode)")
+                }
+            }
         }
     }
-
 }
